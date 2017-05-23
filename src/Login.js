72b82cd8ff7fb258502946e7729ddabee79b1cea
Will Cameron <cameron.will@gmail.com>
2017-05-23 10:45:07 -0700

    add login

---
+++ src/Login.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {
+  Button,
+  Container as RContainer,
+  Input
+} from 'rebass';
+
+const Login = ({
+    email,
+    password,
+    firstName,
+    lastName,
+    error,
+    onInputChange,
+    authenticate,
+    createUser
+}) => {
+    return (
+        <RContainer>
+            <Input value={email}   
+                    label="Email"
+                    name="email"
+                    onChange={onInputChange} />
+            <Input value={password}
+                    type="password" 
+                    label="Password"
+                    name="password"
+                    onChange={onInputChange} />
+
+            <Button backgroundColor="secondary" 
+                    onClick={authenticate}>
+                Login
+            </Button>
+            
+            <Input label="First Name"
+                    name="firstName"
+                    value={firstName}
+                    onChange={onInputChange} />
+            <Input label="Last Name"
+                    name="lastName"
+                    value={lastName}
+                    onChange={onInputChange} />
+            <Button backgroundColor="secondary" 
+                    onClick={createUser}>
+                Register
+            </Button>
+            { error ? error : null }
+        </RContainer>
+    );
+}
+
+export default Login;
